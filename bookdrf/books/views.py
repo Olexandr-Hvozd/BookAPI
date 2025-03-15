44f6956d1030756985555e312d76e0e3abaf9b09
Olexandr-Hvozd <olexandr.hvozd@gmail.com>
2025-03-15 09:27:49 +0200

    Add search by cat

--- bookdrf/books/views.py
+++ bookdrf/books/views.py
@@ -7,11 +7,27 @@ from rest_framework.response import Response
 
 
 class BookViewSet(viewsets.ModelViewSet):
+    """
+    This viewset provides CRUD operations for the Book model.
+    It includes extra actions for searching books by author, title, and category.
+    """
     queryset = Books.objects.all()
     serializer_class = BookSerializer
         
     @action(methods=['get'], detail=False, url_path='author/(?P<authors>[^/]+)')
     def author(self, request, authors=None):
+        """
+        This action allows you to get books by a specific author.
+
+        URL Example:
+        /api/v1/books/author/author_name/
+
+        Parameters:
+        - authors (str): The name of the author you want to search for.
+
+        Returns:
+        - A list of books written by the specified author.
+        """
         author = Books.objects.filter(author=authors)
 
         if not author.exists():
@@ -23,6 +39,18 @@ class BookViewSet(viewsets.ModelViewSet):
     
     @action(methods=['get'], detail=False, url_path='search-by-title')
     def search_by_title(self, request):
+        """
+        This action allows you to search for a book by its title.
+
+        URL Example:
+        /api/v1/books/search-by-title/?title=Book Title
+
+        Parameters:
+        - title (str): The title of the book you want to search for.
+
+        Returns:
+        - A book object if the title is found, or an error message if the book is not found.
+        """
         title = request.query_params.get('title', None)
 
         if not title:
@@ -38,5 +66,33 @@ class BookViewSet(viewsets.ModelViewSet):
 
         serializer = BookSerializer(book)
         return Response(serializer.data)
+
+    @action(methods=['get'], detail=False, url_path='search-by-cat')
+    def search_by_cat(self, request):
+        """
+        This action allows you to search for books by category.
+
+        URL Example:
+        /api/v1/books/search-by-cat/?cat=1
+
+        Parameters:
+        - cat (int): The ID of the category to search for.
+
+        Returns:
+        - A list of books from the specified category, or an error if the category doesn't exist.
+        """
+        cat = request.query_params.get('cat', None)
+
+        if not cat:
+            return Response({'details': "name parameter is required"})
+        
+        books = Books.objects.filter(cat=cat)
+
+        if not books:
+            return Response({'detail': 'category not found with this name'})
+        
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
 
 
